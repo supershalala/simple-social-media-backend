dac319e31743c71b1fffccf49ad1057fea4fb771
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-06-19 15:39:40 +1000

    init setup and commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
