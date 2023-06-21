235788a3b529c8d258c5d29a73bb043aed6075a5
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-06-21 16:46:01 +1000

    add and delete friends to user in userController

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,7 +4,9 @@ const {
   getUserById,
   createUser,
   updateUser,
-  deleteUser
+  deleteUser,
+  addFriend,
+  removeFriend
 } = require('../../controllers/userController');
 
 // /api/users
@@ -17,5 +19,10 @@ router.route('/:id')
   .get(getUserById)
   .put(updateUser)
   .delete(deleteUser);
+
+  // /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(removeFriend);
 
 module.exports = router;
